0ef33839fc2c9c6c9080b7707611f1fcaf9d7823
lakpafutilama <lakpa.lama4433@gmail.com>
2023-05-28 13:05:30 +0545

    timezone to local

--- utils/morganLogger.js
+++ utils/morganLogger.js
@@ -1,5 +1,6 @@
 const morgan = require("morgan");
 const fs = require("fs");
+const moment = require("moment-timezone");
 
 const accessLogStream = fs.createWriteStream("./access.log", {
   flags: "a",
@@ -14,7 +15,7 @@ const accessLogStream = fs.createWriteStream("./access.log", {
 
 // write in file and console the log
 const logger = morgan(
-  ":method :status :url :user-agent  - :response-time ms  [:date[web]]",
+  ":method :status :url :user-agent  - :response-time ms  [:date[Asia/Kathmandu]]",
   {
     stream: {
       write: function (str) {
@@ -24,5 +25,10 @@ const logger = morgan(
     },
   }
 );
+
+// setting local timezone
+morgan.token("date", (req, res, tz) => {
+  return moment().tz(tz).format("YYYY-MM-DD HH:mm:ss");
+});
 
 module.exports = logger;
